028c3c89c203954ca9d4721645fffd990189cf53
Peter <naughtnowwhen@gmail.com>
2018-07-17 14:49:18 -0700

    begin working on dirtybranch

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // 1st and Pike	23	65	6.3
 // SeaTac Airport	3	24	1.2
 // Seattle Center	11	38	3.7
@@ -18,33 +16,12 @@ var pikeLocation = {
 
   pikeCustomerPerHour : [],
   pikeCookieAmountPerHour : [],
-  pikeCookieTotal : 0,
+  pikeCookieTotal : [],
 
   pikeRandomCustAmount: function (){
     return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)
      + this.minCustomer);
   },
-};
-
-// example 
-// function Store (storeName, customerPerHour, cookiesPerHour, totalCookies) {
-//   this.storeName = storeName;
-//   this.customerPerHour = customerPerHour;
-//   this.cookiesPerHour = cookiesPerHour;
-//   this.totalCookies = totalCookies;
-//    allOf.push(this);
-// }
-
-function Store (storeName, minCustomer, maxCustomer, avgEat) {
-  this.storeName = storeName;
-  this.minCustomer = minCustomer;
-  this.maxCustomer = maxCustomer;
-
-  this.avgEat = avgEat;
-}
-Store.prototype.RandomCustAmount = function() {
-  return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)
-     + this.minCustomer);  
 };
 
 
@@ -53,31 +30,28 @@ for (var i = 0; i < 14; i++) {
   pikeLocation.pikeCookieAmountPerHour.push(Math.floor(pikeLocation.pikeRandomCustAmount() * pikeLocation.avgEat)) && pikeLocation.pikeCookieTotal.push(pikeLocation.pikeCookieAmountPerHour[i]);
 }
 
-// var ulEl = document.createElement('ul');
-// for (i = 0; i < 14; i++) {
-//   var liEl = document.createElement('li');
-//   liEl.textContent = pikeLocation.pikeCustomerPerHour[i];
 
-//   ulEl.appendChild(liEl);
-
-//   console.log(ulEl);
-
-var ulEl = document.createElement('ul');
-var element = document.createElement('div1');
-
-// for (var j = 0; j < 3;j++ ){
-// want to iterate through j 3 times so automate this create text on html for my custperhour cookies per hour and total cookies
-for (i = 0; i < 14; i++) {
-  var liEl = document.createElement('li');
-//   var nodePikeCustomerPerHour = document.createTextNode(pikeLocation.pikeCustomerPerHour[i]);
-  ulEl.appendChild(liEl);
-  liEl.appendChild(nodePikeCustomerPerHour);
-  var nodePikeCookiesPerHourTextArr = [];
-  var nodePikeCookiesPerHourText = document.createTextNode(pikeLocation.pikeCookieTotal[i]);
-  liEl.appendChild(nodePikeCookiesPerHourText);
-
-//   element.appendChild(liEl);
-  console.log(ulEl);
-}
+// for (var j = 0; j < 14; j++) {
+//   pikeLocation.pikeCookieAmountPerHour.push(Math.floor(pikeLocation.pikeRandomCustAmount() * pikeLocation.avgEat))
+// }
 
 
+
+
+// for (var j = 0; j < pikeLocation.pikeCookieAmountPerHour; j++) {
+//   pikeLocation.pikeTime[j] * pikeLocation.avgEat;
+// }
+
+
+
+
+
+// var pikeRandomCustAmount = function (minCustomer, maxCustomer){
+//   return Math.floor(Math.random() * (maxCustomer - minCustomer)
+//      + minCustomer);
+// };
+// var minCustomer = 23;
+// var maxCustomer =  65;
+
+// var x = 12;
+// pikeRandomCustAmount
