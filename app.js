df7cb1aafcbec92e0cfd27bba23bee063192e49f
Peter <naughtnowwhen@gmail.com>
2018-07-16 22:14:51 -0700

    dipslaying javascript on html

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // 1st and Pike	23	65	6.3
 // SeaTac Airport	3	24	1.2
 // Seattle Center	11	38	3.7
@@ -29,6 +31,32 @@ for (var i = 0; i < 14; i++) {
   pikeLocation.pikeCustomerPerHour.push(pikeLocation.pikeRandomCustAmount()) &&
   pikeLocation.pikeCookieAmountPerHour.push(Math.floor(pikeLocation.pikeRandomCustAmount() * pikeLocation.avgEat)) && pikeLocation.pikeCookieTotal.push(pikeLocation.pikeCookieAmountPerHour[i]);
 }
+
+var ulEl = document.createElement('ul');
+for (i = 0; i < 14; i++) {
+  var liEl = document.createElement('li');
+  liEl.textContent = pikeLocation.pikeCustomerPerHour[i];
+  ulEl.appendChild(liEl);
+
+  //example from w3 that works
+  var para = document.createElement("p");
+  var node = document.createTextNode(pikeLocation.pikeCustomerPerHour[i]);
+  para.appendChild(node);
+  var element = document.getElementById("div1");
+  element.appendChild(para);
+  
+// 
+}
+
+// var ulEl = document.createElement('ul');
+
+// for(var i = 0; i < months.length; i++) {
+//   var liEl = document.createElement('li');
+//   liEl.textContent = months[i];
+//   ulEl.appendChild(liEl);
+// }
+
+
 
 
 // for (var j = 0; j < 14; j++) {
