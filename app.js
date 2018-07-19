c9897546914fb1bf3c070cadea9538aaa9d558b1
Peter <naughtnowwhen@gmail.com>
2018-07-19 07:57:13 -0700

    first baby step working on eventlistern.

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 
 'use strict';
 
+
+// working on event listeners.
+var firstDiv = document.getElementById('firstBodyDiv');
+var CookieQuestion = document.getElementById('ask');
+var choiceP = document.getElementById('ask');
+
+CookieQuestion.addEventListener('click', /* a function needs to go here */  {
+  firstDiv.document.getElementById('ask').innerHTML = 'Great, we can help! Salmon is rich in nutrition. We make it easy to get all that health goodness into a food vehicle you\'ll want to eat.';});
+
+
+
+
+
 // var tbEl;
 
 // what about storing inside an object instead?
---
+++ app1.js
@@ -0,0 +1,14 @@
+// working on event listeners.
+var firstDiv = document.getElementById('firstBodyDiv');
+var CookieQuestion = document.getElementById('ask');
+var choiceP = document.getElementById('ask');
+
+CookieQuestion.addEventListener('click', function() {
+  firstDiv.document.getElementById('ask').innerHTML = 'Great, we can help! Salmon is rich in nutrition. We make it easy to get all that health goodness into a food vehicle you\'ll want to eat.';});
+
+// or we could do it with the .ChangeText method? are there different use cases between them?
+function changeText() {
+  CookieQuestion.textContent = CookieQuestion.textContent += 'Great, we can help! Salmon is rich in nutrition. We make it easy to get all that health goodness into a food vehicle you\'ll want to eat.';
+
+};
+changeText();
